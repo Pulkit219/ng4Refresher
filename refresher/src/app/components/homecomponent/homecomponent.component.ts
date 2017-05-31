3026f98b496cd3a88b69a84a621c03955e24956a
Pulkit219 <ascension1110@gmail.com>
2017-05-31 19:09:24 -0400

    4th comm

--- refresher/src/app/components/homecomponent/homecomponent.component.ts
+++ refresher/src/app/components/homecomponent/homecomponent.component.ts
@@ -9,7 +9,27 @@ export class HomecomponentComponent implements OnInit {
 
   constructor() { }
 
-  ngOnInit() {
+// public listOfGuys = [
+// {  name:"Pulkit"},
+// {  name:"Jordan"},
+// {  name:"RIcky"},
+// {  name:"John"}
+    public shoppingListItems = [{name:"Pulkit"}, {name:"Calem"}, {name:"Francois"}];
+    public selectedItem = {name:""};
+
+onItemClicked(shoppingListItem){
+this.selectedItem = shoppingListItem;
+}
+addItem(shoppingListItem){
+this.shoppingListItems.push({name: shoppingListItem.value});
+}
+removeItem(){
+this.shoppingListItems.shift();
+}
+
+
+
+ ngOnInit() {
   }
 
 }
