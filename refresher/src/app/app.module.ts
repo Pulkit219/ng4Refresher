2351c3cfd676bb297391304bd5ef4e3f0ae1d83b
Pulkit219 <ascension1110@gmail.com>
2017-05-31 10:54:03 -0400

    2ND COMMIT
    ROUTING SUCCESS!!

--- refresher/src/app/app.module.ts
+++ refresher/src/app/app.module.ts
@@ -2,19 +2,39 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+// import { ActivatedRoute } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
+// import { provideRoutes} from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { HomecomponentComponent } from './components/homecomponent/homecomponent.component';
+import { AboutcomponentComponent } from './components/aboutcomponent/aboutcomponent.component';
+
+
+const appRoutes: Routes = [
+  { path: 'home', component: HomecomponentComponent },
+  { path: 'about',component: AboutcomponentComponent },
+  { path: '',
+    redirectTo: '/home',
+    pathMatch: 'full'
+  },
+  
+];
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    HomecomponentComponent,
+    AboutcomponentComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+     RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
